93cd39980363c47511796b232f82ea5de9487f84
Jason Saal <jasonjerohamsaal@gmail.com>
2025-01-22 17:20:53 +0200

    Create App.tsx

---
+++ PortfolioGit/src/App.tsx
@@ -0,0 +1,34 @@
+import { FooterSocial } from "./Components/FooterSocial"
+import { Routes, Route } from "react-router-dom"
+import { HeaderSimple } from "./Components/HeaderSimple"
+import Home from "./Components/Home"
+import Details from "./Components/Details"
+import Hobbies from "./Components/Hobbies"
+import ContactMe from "./Components/ContactMe"
+import { MantineProvider, useMantineColorScheme, Button, Group } from '@mantine/core'
+
+function App() {
+  const { setColorScheme, clearColorScheme } = useMantineColorScheme();
+  return (
+    <div>
+       <MantineProvider>
+       <HeaderSimple/>
+       <Routes>
+       <Route path="/" element={<Home />}/>
+       <Route path="/details" element={<Details />}/>
+       <Route path="/Hobbies" element={<Hobbies />}/>
+       <Route path="/ContactMe" element={<ContactMe />}/>
+       </Routes>
+       <FooterSocial/>
+       <Group>
+      <Button onClick={() => setColorScheme('light')}>Light</Button>
+      <Button onClick={() => setColorScheme('dark')}>Dark</Button>
+      <Button onClick={() => setColorScheme('auto')}>Auto</Button>
+      <Button onClick={clearColorScheme}>Clear</Button>
+      </Group>
+       </MantineProvider> 
+    </div>
+  )
+}
+
+export default App
